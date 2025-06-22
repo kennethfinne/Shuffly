716364a2b590b9546d2a3df1974f671451868c61
Kenneth Finne <kenneth.finne@gmail.com>
2025-06-22 21:22:38 +0200

    Update deprecated code

--- app/src/main/java/com/kfinne/shuffleswap/MainActivity.kt
+++ app/src/main/java/com/kfinne/shuffleswap/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.kfinne.shuffleswap
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.launch
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,14 +16,51 @@ class MainActivity : AppCompatActivity() {
     // Request code for auth activity
     private val AUTH_REQUEST_CODE = 1001
 
+    // 1. Register the ActivityResultLauncher
+    private lateinit var authActivityLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         // Don't set content view since we're navigating away immediately
-
         supportActionBar?.hide()
+
+        // Initialize the launcher
+        authActivityLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult() // Prebuilt contract
+        ) { result -> // This is your callback for the result
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+                val token = data?.getStringExtra("ACCESS_TOKEN")
+                if (token != null) {
+                    accessToken = token
+                    onAuthenticationSuccess(token)
+                } else {
+                    showAuthError("No access token received")
+                    finish()
+                }
+            } else if (result.resultCode == Activity.RESULT_CANCELED) {
+                val data: Intent? = result.data
+                val error = data?.getStringExtra("AUTH_ERROR")
+                if (error != null) {
+                    showAuthError(error)
+                } else {
+                    showAuthCancelled()
+                }
+                finish()
+            }
+        }
+
         checkAuthenticationAndNavigate()
     }
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        // Don't set content view since we're navigating away immediately
+//
+//        supportActionBar?.hide()
+//        checkAuthenticationAndNavigate()
+//    }
 
     private fun checkAuthenticationAndNavigate() {
         val sharedPrefs = getSharedPreferences(AuthActivity.PREFS_NAME, MODE_PRIVATE)
@@ -46,40 +87,41 @@ class MainActivity : AppCompatActivity() {
 
     private fun startLoginForResult() {
         val intent = Intent(this, AuthActivity::class.java)
-        startActivityForResult(intent, AUTH_REQUEST_CODE)
+        //startActivityForResult(intent, AUTH_REQUEST_CODE)
+        authActivityLauncher.launch(intent)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == AUTH_REQUEST_CODE) {
-            when (resultCode) {
-                RESULT_OK -> {
-                    // Authentication successful
-                    val token = data?.getStringExtra("ACCESS_TOKEN")
-                    if (token != null) {
-                        accessToken = token
-                        onAuthenticationSuccess(token)
-                    } else {
-                        showAuthError("No access token received")
-                        // Close the app or handle error appropriately
-                        finish()
-                    }
-                }
-                RESULT_CANCELED -> {
-                    // User cancelled authentication
-                    val error = data?.getStringExtra("AUTH_ERROR")
-                    if (error != null) {
-                        showAuthError(error)
-                    } else {
-                        showAuthCancelled()
-                    }
-                    // Close the app since user cancelled login
-                    finish()
-                }
-            }
-        }
-    }
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//
+//        if (requestCode == AUTH_REQUEST_CODE) {
+//            when (resultCode) {
+//                RESULT_OK -> {
+//                    // Authentication successful
+//                    val token = data?.getStringExtra("ACCESS_TOKEN")
+//                    if (token != null) {
+//                        accessToken = token
+//                        onAuthenticationSuccess(token)
+//                    } else {
+//                        showAuthError("No access token received")
+//                        // Close the app or handle error appropriately
+//                        finish()
+//                    }
+//                }
+//                RESULT_CANCELED -> {
+//                    // User cancelled authentication
+//                    val error = data?.getStringExtra("AUTH_ERROR")
+//                    if (error != null) {
+//                        showAuthError(error)
+//                    } else {
+//                        showAuthCancelled()
+//                    }
+//                    // Close the app since user cancelled login
+//                    finish()
+//                }
+//            }
+//        }
+//    }
 
     private fun onAuthenticationSuccess(token: String) {
         // Show success toast
